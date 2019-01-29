285425673eecb1c5d352f4b53db02aee83feae7c
Eunice Min <euniceem1007@gmail.com>
2019-01-28 15:05:55 -1000

    working on post heard

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 app.use(express.static('public'));
 
 const data = {
@@ -14,43 +16,88 @@ const data = {
   "totalScore": 0
 };
 
+
+
 // GET /
 app.get('/', (req, res) => {
-  res.status(200);
-  res.sendFile('index');
+  res.status(200).sendFile('index');
 });
 
 //GET /buzzwords
 app.get('/buzzwords', (req, res) => {
-  res.json(data);
-})
+  res.status(200).json(data);
+});
 
 //POST /buzzwords
 app.post('/buzzwords', (req, res) => {
   let body = req.body;
+  let dataBody = data.buzzWords;
 
-  if (!typeof body.buzzWord === 'string' || typeof body.points === 'number') {
-    res.status(500);
-    return res.send(`{Error: Please provide a valid buzzword and points}`)
+  if (!typeof body.buzzWord === 'string' &&
+    (!typeof body.points === 'number') &&
+    dataBody.length < 5) {
+    return res.status(500).send(`{Error: Please provide a valid buzzword and points}`)
   } else {
     let newBody = {};
     newBody.buzzWord = body.buzzWord;
     newBody.points = parseInt(body.points);
     data.buzzWords.push(newBody);
-    res.status(200);
-    res.send(`{"success": true}`);
+    res.status(200).send(`{"success": true}`);
   }
-})
+});
 
 //PUT /buzzwords
 app.put('/buzzwords', (req, res) => {
   let body = req.body;
-  console.log("BODY: ", body);
-  if (typeof body.buzzWord === 'string') {
+  let dataBody = data.buzzWords;
 
+  for (var i = 0; i < dataBody.length; i++) {
+
+    if (typeof body.buzzWord === 'string' &&
+      body.buzzWord === dataBody[i].buzzWord &&
+      dataBody.length < 5) {
+      dataBody[i].points = parseInt(body.points);
+      res.status(200).send(`{"success": true}`);
+    } else {
+      return res.status(500).send(`{Error: Please provide a valid buzzword and points}`);
+    }
   }
+});
 
-})
+//DELETE /buzzwords
+app.delete('/buzzwords', (req, res) => {
+  let body = req.body;
+  let dataBody = data.buzzWords;
+
+  for (var i = 0; i < dataBody.length; i++) {
+
+    if (dataBody[i].buzzWord.includes(body.buzzWord)) {
+      dataBody.splice(i, 1);
+      res.status(200).send(`{"success": true}`);
+    } else {
+      return res.status(500).send(`{Error: The buzzword you provided does not exist}`);
+    }
+  }
+});
+
+//POST /reset
+app.post('/reset', (req, res) => {
+  let dataBody = req.body;
+
+  for (var i = 0; i < dataBody.length; i++) {
+    if (dataBody.length === 0) {
+      return res.status(500).send(`{Error: There are no buzzwords to clear}`);
+    } else {
+      dataBody.splice(dataBody.length);
+      res.status(200).send(`{"success": true}`);
+    }
+  }
+});
+
+//POST /heard
+app.post('/heard', (req, res) => {
+
+});
 
 
 const server = app.listen(3000, () => {
